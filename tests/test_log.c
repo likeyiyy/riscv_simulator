b017f359bdfe4ad32c94679cfd1020a475b9c5f6
likeyiyy <keyi.lee@gllue.com>
2024-06-09 15:25:16 +0800

    update

--- tests/test_log.c
+++ tests/test_log.c
@@ -15,22 +15,13 @@ char buffer[MAX_LINES][LINE_LENGTH];
 int buffer_start = 0;
 int buffer_count = 0;
 
-// 自定义函数，将格式化字符串写入指定文件
-void fprint_to_file(const char *filename, const char *format, ...) {
-    FILE *file = fopen(filename, "a"); // 以追加模式打开
-    if (file == NULL) {
-        perror("fopen");
-        return;
+void* ncurses_thread(void* arg) {
+    FILE *file = fopen("output.log", "r");
+    if (!file) {
+        fprintf(stderr, "Error: Unable to open file for reading.\n");
+        return NULL;
     }
 
-    va_list args;
-    va_start(args, format);
-    vfprintf(file, format, args);
-    va_end(args);
-    fclose(file);
-}
-
-void* ncurses_thread(void* arg) {
     initscr();
     noecho();
     cbreak();
@@ -41,19 +32,9 @@ void* ncurses_thread(void* arg) {
     box(win, 0, 0);                     // 绘制边框
     wrefresh(win);
 
-    FILE *file;
     long last_pos = 0; // 上次读取的位置
 
     while (1) {
-        file = fopen("output.log", "r");
-        if (!file) {
-            mvwprintw(win, 1, 1, "Error: Unable to open file.");
-            wrefresh(win);
-            getch();
-            endwin();
-            return NULL;
-        }
-
         // 移动到上次读取的位置
         fseek(file, last_pos, SEEK_SET);
 
@@ -82,29 +63,39 @@ void* ncurses_thread(void* arg) {
         }
 
         wrefresh(win);
-        fclose(file);
         usleep(500000); // 0.5秒间隔更新
     }
 
     endwin();
+    fclose(file);
     return NULL;
 }
 
 int main() {
     pthread_t thread;
 
-    fprint_to_file("output.log", "This will be written to output.log\n");
+    // 打开文件
+    FILE *file = fopen("output.log", "a+");
+    if (!file) {
+        perror("fopen");
+        return 1;
+    }
+
+    fprintf(file, "This will be written to output.log\n");
+    fflush(file); // 确保输出立即写入文件
 
     // 启动 ncurses 线程
     pthread_create(&thread, NULL, ncurses_thread, NULL);
 
     // 主线程继续运行其他任务
     for (int i = 0; i < 100; ++i) {
-        fprint_to_file("output.log", "Log entry %d\n", i);
-        fflush(stdout); // 确保输出立即写入文件
-        sleep(0.1); // 模拟日志生成间隔
+        fprintf(file, "Log entry %d\n", i);
+        fflush(file); // 确保输出立即写入文件
+        sleep(1); // 模拟日志生成间隔
     }
 
     pthread_join(thread, NULL);
+    fclose(file); // 关闭文件
+
     return 0;
 }
