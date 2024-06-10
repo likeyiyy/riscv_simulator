f2dc70e129e99aa1c4478fa7a20b37a59e0507e1
Keyi.lee <keyi.lee@gllue.com>
2024-06-10 20:52:43 +0800

    update

--- src/uart.c
+++ src/uart.c
@@ -13,10 +13,8 @@ void uart_init(UART *uart) {
     uart->registers[LSR] = LSR_THRE; // Initialize LSR with THR empty
 }
 
-void uart_write(uint64_t addr, uint64_t value, uint32_t size) {
+void uart_write(uint64_t offset, uint64_t value, uint32_t size) {
     UART *uart = get_uart();
-    uint64_t offset = addr - UART_BASE_ADDR;
-    mfprintf("UART write: addr: 0x%x, value: 0x%x, offset: 0x%x\n", addr, value, offset);
 
     if (offset < sizeof(uart->registers)) {
         if (uart->registers[LCR] & 0x80 && (offset == DLL || offset == DLM)) {
@@ -32,9 +30,8 @@ void uart_write(uint64_t addr, uint64_t value, uint32_t size) {
     }
 }
 
-uint64_t uart_read(uint64_t addr, uint32_t size) {
+uint64_t uart_read(uint64_t offset, uint32_t size) {
     UART *uart = get_uart();
-    uint64_t offset = addr - UART_BASE_ADDR;
     if (offset < sizeof(uart->registers)) {
         return uart->registers[offset];
     }
